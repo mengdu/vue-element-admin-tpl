848800493bd4260dc51459affa25ae4eb611cc6e
lyg <ygl@szmsmk.com.cn>
2018-01-17 19:43:22 +0800

    新增input组件

---
+++ src/m/input/README.md
@@ -0,0 +1,24 @@
+## Input
+
+Input 输入框。
+
+```html
+<m-input placeholder="输入框">
+```
+
+
+
+### Input Attributes
+
++ disabled 禁用状态
++ type 类型input, text, password, textarea
++ size 大小 max, large, 默认, samll, mini
++ theme danger,success
++ placeholder 
++ block 块
++ maxHeight
++ maxWidth
++ minHeight
++ minWidth
++ width
++ height
--- src/pages/c-input.vue
+++ src/pages/c-input.vue
@@ -9,7 +9,26 @@
   </div>
 <m-box>
 
-<m-input placeholder="输入框" />
+<m-input placeholder="输入框" v-model="text"/> <m-button>test</m-button>
+<p></p>
+<m-input placeholder="输入框" v-model="text" theme="danger" size="small"/> <m-button size="small">测试</m-button>
+<p></p>
+<m-input placeholder="输入框" theme="success"/>
+<p></p>
+<m-input block></m-input>
+<p></p>
+
+<m-input placeholder="输入框" type="textarea" min-width="250px"/>
+
 </m-box>
 </div>
 </template>
+<script type="text/javascript">
+export default {
+  data () {
+    return {
+      text: 'default'
+    }
+  }
+}
+</script>
