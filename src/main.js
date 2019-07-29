d5ff0f4f87398c63042a3718419df798b1255d1a
ygl <lanyueos@qq.com>
2019-07-29 17:21:33 +0800

    添加 less 支持

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { isProd } from './utils'
+import './assets/css/main.less'
 
 Vue.config.productionTip = false
 
