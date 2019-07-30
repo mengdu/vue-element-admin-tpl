1788703f19d04b39bd0859afd8102829624f424c
ygl <lanyueos@qq.com>
2019-07-30 14:21:30 +0800

    添加 nprogress

--- src/main.js
+++ src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
+import Nprogress from 'nprogress'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import { isProd } from './utils'
 import './assets/css/main.less'
+import 'nprogress/nprogress.css'
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, form, next) => {
+  Nprogress.start()
+
+  next()
+})
+
+router.afterEach(transition => {
+  setTimeout(() => {
+    Nprogress.done()
+  })
+})
 
 const app = new Vue({
   router,
