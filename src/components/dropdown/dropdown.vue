4f38207816a38bc0afb63aa81a38b706cbc5d96b
ygl <lanyueos@qq.com>
2019-07-31 12:28:05 +0800

    添加 dropdown 组件

---
+++ src/components/dropdown/dropdown.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="m-dropdown"
+    @click="handleClick"
+    >
+    <slot></slot>
+    <transition name="m-dropdown">
+      <div
+        class="m-dropdown-panel"
+        :class="{
+          'is-center': align === 'center',
+          'is-right': align === 'right',
+          'is-left': align === 'left'
+        }"
+        :style="{
+          margin: margin,
+          ...panelStyle
+        }"
+        @click.stop=""
+        v-show="isOpen">
+        <slot name="panel"></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    trigger: {
+      type: String,
+      default: 'click'
+    },
+    align: {
+      type: String
+    },
+    margin: String,
+    panelStyle: Object
+  },
+  data () {
+    return {
+      isOpen: false
+    }
+  },
+  methods: {
+    handleClick () {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
+</script>
---
+++ src/components/dropdown/index.js
@@ -0,0 +1,4 @@
+import Dropdown from './dropdown'
+import './dropdown.less'
+
+export default Dropdown
