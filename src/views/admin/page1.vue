37f242a5af0d5a18c1d62b1ea40696f3e2c51595
jane <lanyueos@qq.com>
2020-09-02 22:40:00 +0800

    feat: 调整表头样式

--- src/views/admin/page1.vue
+++ src/views/admin/page1.vue
@@ -6,16 +6,18 @@
     </el-breadcrumb>
     <div class="block-box">
       <div class="block-box--header">Users</div>
-      <el-table :data="dataList" v-loading="loadding" stripe border>
-        <el-table-column label="ID" prop="id"></el-table-column>
-        <el-table-column label="Avatar">
-          <template slot-scope="scope"><img :src="scope.row.avatar_url" alt="avatar" width="32" height="32" style="vertical-align: middle;"/></template>
-        </el-table-column>
-        <el-table-column label="Username" prop="login"></el-table-column>
-        <el-table-column label="node_id" prop="node_id"></el-table-column>
-        <el-table-column label="Home" prop="html_url"></el-table-column>
-        <el-table-column label="Type" prop="type"></el-table-column>
-      </el-table>
+      <div class="block-box--content">
+        <el-table :data="dataList" v-loading="loadding" stripe border>
+          <el-table-column label="ID" prop="id"></el-table-column>
+          <el-table-column label="Avatar">
+            <template slot-scope="scope"><img :src="scope.row.avatar_url" alt="avatar" width="32" height="32" style="vertical-align: middle;"/></template>
+          </el-table-column>
+          <el-table-column label="Username" prop="login"></el-table-column>
+          <el-table-column label="node_id" prop="node_id"></el-table-column>
+          <el-table-column label="Home" prop="html_url"></el-table-column>
+          <el-table-column label="Type" prop="type"></el-table-column>
+        </el-table>
+      </div>
       <div class="block-box--footer">
         Footer
       </div>
